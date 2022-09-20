d736364cc46a780f5f1fb9fe9fef38e07582689f
abadifaizal <abadifaizal@gmail.com>
2022-09-20 23:31:17 +0900

    writing javascript

--- js-calculator/js/script.js
+++ js-calculator/js/script.js
@@ -1,1 +1,23 @@
-console.log("works!")
+let totalCalc = 0;
+let initialState = 0;
+let previousOperator;
+const screenEl = document.querySelector('.screen');
+const calcBtnEl = document.querySelectorAll('.calc-button');
+
+calcBtnEl.forEach(btnEl => {
+  btnEl.addEventListener('click', function handleClick(event) {
+    console.log(event.target.textContent);
+    checkBtnValue(event.target.textContent);
+  });
+});
+
+function checkBtnValue(value) {
+  if(isNaN(parseInt(value))) {
+    console.log(value, 'Is not a number');
+    // handle Symbol
+  } else {
+    console.log(value, 'Is a number');
+    // handle Number
+  }
+}
+
